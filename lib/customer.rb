0d8e5d44cc8522f354912b33e7992250f173788d
alex.szeto <alex.szeto.a@gmail.com>
2020-01-12 02:40:42 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+    @@all = []
+
+    attr_accessor :age, :meals, :waiters
+    attr_reader :name
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+        @meals = []
+        @waiters = []
+        return
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        new_meal = Meal.new(waiter,self,total, tip)
+        @waiters << waiter
+        @meals << new_meal
+        waiter.meals << new_meal
+        #puts waiter
+    end
+
 end
